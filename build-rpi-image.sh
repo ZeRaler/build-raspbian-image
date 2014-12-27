4958168195b4182b955ade134c392a5fbd12c1ff
ZeRaler <zeraler@gmail.com>
2014-12-27 16:54:45 +0100

    Limiting entropy needs

--- build-rpi-image.sh
+++ build-rpi-image.sh
@@ -359,7 +359,7 @@ echo '  Collecting entropy ...' >> /dev/kmsg
 # Drain entropy pool to get rid of stored entropy after boot.
 dd if=/dev/urandom of=/dev/null bs=1024 count=10 2>/dev/null
 
-while entropy=\$(cat /proc/sys/kernel/random/entropy_avail); [ \$entropy -lt 200 ]
+while entropy=\$(cat /proc/sys/kernel/random/entropy_avail); [ \$entropy -lt 100 ]
     do sleep 1
 done
 
